05803af69dd5845f0f6592501a676054c4e4f6b7
quincyshadow <inversenormal@gmail.com>
2019-07-02 11:40:03 -0700

    .

---
+++ shopping-cart/components/CartItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ColorComponent extends React.Component {
+
+  render() {
+    return (
+        <div className="container">
+        <h1>Items</h1>
+      </div>
+    )
+  }
+
+}
